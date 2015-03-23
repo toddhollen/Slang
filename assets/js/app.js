5e34a01aff3df5ae9992eba579f084cbce0d1088
toddhollen <toddhollen@gmail.com>
2015-03-22 18:36:15 -0600

    Updated answers and images

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,7 @@
         choices: ["Helmet", "The Goal", "The Bench", "The Rink"],
         qNum : 0,
         correct : 0,
-        correctImage : ["assets/images/gameanswers/helmet.jpg"],
+        correctImage : ["assets/images/gameanswers/helmet.gif"],
         correctDescription: 'Strap on your bucket so you don\'t scramble your brains.'
         },
         {
@@ -15,7 +15,7 @@
         choices: ["Zamboni","Skates", "Stick", "Hockey Groupies"],
         qNum : 1,
         correct : 1,
-        correctImage : ["assets/images/gameanswers/skates.jpg"],
+        correctImage : ["assets/images/gameanswers/skates.gif"],
         correctDescription:'Tie on your blades and let\'s hit the ice.'
         },
         {
@@ -23,15 +23,15 @@
         choices: ["Hockey Groupie", "A sick goalie mask", "An impressive play OR a player that’s talented both on the ice and off.", "Freshly Zambonied Ice" ],
         qNum : 2,
         correct : 2,
-        correctImage : ["assets/images/gameanswers/beauty.jpg"],
-        correctDescription:'That bar-down snipe was a real beauty. <br><br>That guy has sick flow and greasy hands, he is a beuaty.'
+        correctImage : ["assets/images/gameanswers/beauty.gif"],
+        correctDescription:'That spin-0-rama was a real beauty. <br><br>That guy has sick flow and greasy hands, he is a beauty.'
         },
         {
         question: "Flow",
-        choices: ["Great skating technique", "Momentum durring a game", "Beers in the parking lot after the game", "Great hockey hair, typically long that flows out of the helmet when the player skates."],
+        choices: ["Great skating technique", "Momentum during a game", "Beers in the parking lot after the game", "Great hockey hair, typically long that flows out of the helmet when the player skates."],
         qNum : 3,
         correct : 3,
-        correctImage : ["assets/images/gameanswers/flow.jpg"],
+        correctImage : ["assets/images/gameanswers/flow.gif"],
         correctDescription:'Check out that beauty\'s flow.' 
         },
         {
@@ -44,7 +44,7 @@
         },
         {
         question: "Gongshow",
-        choices: ["Between period entertainment","A complete mess of a game or the sheneigans that take place after the game.", "When someone scores a hat trick", "A fight"],
+        choices: ["Between period entertainment","A complete mess of a game or the shenanigans that take place after the game.", "When someone scores two hat tricks.", "A fight"],
         qNum : 5,
         correct : 1,
         correctImage : ["assets/images/gameanswers/gongshow1.gif"],
@@ -55,108 +55,108 @@
         choices: ["When a player forgets to wear a cup.", "A big save", "A great pass", "A variety of moves where a player fakes out a goalie or player"],
         qNum : 6,
         correct : 3,
-        correctImage : ["assets/images/gameanswers/dangle.gif"],
-        correctDescription:'That was a beauty dangle. I call it the \'Tripple deke.\' <br><br>(Not to be confused with a "danlger," which is the plastic throat guard some goalies attach to their buckets.)'
+        correctImage : ["assets/images/gameanswers/deke.gif"],
+        correctDescription:'That was a beauty dangle. I call it the \'Triple Deke.\' <br><br>(Not to be confused with a "dangler," which is the plastic throat guard some goalies attach to their buckets.)'
         },
         {
         question: "Pillows",
-        choices: ["Goalie Leg Pads"],
+        choices: ["Goalie Leg Pads", "Gloves", "The padding that goes around the inside of the goal.", "Padding in the helmet."],
         qNum : 7,
-        correct : 1,
-        correctImage : ["assets/images/ameanswers/pillows.jpg"],
+        correct : 0,
+        correctImage : ["assets/images/gameanswers/pillows.gif"],
         correctDescription:'A big save off the pillows!'
         },
         {
         question: "Twig",
-        choices: ["A hockey Stick"],
+        choices: ["The boards", "A skinny player", "A weak hit in a fight.", "A hockey stick"],
         qNum : 8,
         correct : 3,
         correctImage : ["assets/images/gameanswers/twig.gif"],
-        correctDescription:''
+        correctDescription:'Look at the flex in his twig when he took that clapper!'
         },
         {
         question: "Where Grandma Keeps the Peanut Butter",
-        choices: ["A goal scored in the top part of the net."],
+        choices: ["A goal scored in the top part of the net.", "A goalie's 5-hole (between his legs)", "A glove save", "The bench"],
         qNum : 9,
         correct : 0,
-        correctImage : ["assets/images/gameanswers/"],
+        correctImage : ["assets/images/gameanswers/grandma.gif"],
         correctDescription:'She put that one top shelf, where grandma keeps the peanut butter!'
         },
         {
         question: "Chirp",
-        choices: ["Trash talking"],
+        choices: ["A cheap hit", "The sound skates make when you stop.", "Trash talking", "A check"],
         qNum : 10,
-        correct : 0,
-        correctImage : ["assets/images/gameanswers/"],
-        correctDescription:''
+        correct : 2,
+        correctImage : ["assets/images/gameanswers/chirp.gif"],
+        correctDescription:'If he keeps chirping the other team\'s goalie like that someone is going to lay him out.'
         },
         {
         question: "Donnybrook",
-        choices: ["A Fight"],
+        choices: ["A hard hit", "A fight", "An old ice rink", "The team's equipment manager"],
         qNum : 11,
-        correct : 0,
-        correctImage : ["assets/images/gameanswers/"],
-        correctDescription:''
+        correct : 1,
+        correctImage : ["assets/images/gameanswers/donnybrook.gif"],
+        correctDescription:'Haven\'t seen a donnybrook like that since Tie Domi and Bob Probert in \'92!'
         },
         {
         question: "Goon",
-        choices: ["A team's enforcer/ a person whose primary role is to fight to protect his teammates"],
+        choices: ["A dumb player", "The referee", "A fan who get's too drunk.", "A team's enforcer/ a person whose primary role is to fight to protect his teammates"],
         qNum : 12,
-        correct : 0,
-        correctImage : ["assets/images/gameanswers/"],
-        correctDescription:''
+        correct : 3,
+        correctImage : ["assets/images/gameanswers/goon.gif"],
+        correctDescription:'The goons are going at it after a missed call on a cheap hit.'
         },
         {
         question: "Bender/ Duster/ Pylon",
-        choices: ["A low skilled player"],
+        choices: ["A low skilled player", "A high skilled player", "A player who chirps a lot.", "The team's equipment manager"],
         qNum : 13,
         correct : 0,
-        correctImage : ["assets/images/gameanswers/"],
-        correctDescription:'Look at this guy, what a duster. <br<br>Bender=Someone whose ankles bend in when he or she skates. <br> Duster=Someone who collects dust sitting on the bench. Pylon= Someone so slow everyone just skates right around him or her.'
+        correctImage : ["assets/images/gameanswers/bender.gif"],
+        correctDescription:'Look at this guy, what a duster. <br<br>Bender=Someone whose ankles bend in when he or she skates. <br>Duster=Someone who collects dust sitting on the bench. <br>Pylon= Someone so slow everyone just skates right around him or her.'
         },
         {
         question: "Bunny/Puck Bunny/Puck",
-        choices: ["A (usually derogotory) term for a hockey groupie"],
+        choices: ["Team mascot", "Good luck charm","A (usually derogatory) term for a hockey groupie", "Zamboni driver"],
         qNum : 14,
-        correct : 0,
-        correctImage : ["assets/images/gameanswers/"],
+        correct : 2,
+        correctImage : ["assets/images/gameanswers/bunny.jpg"],
         correctDescription:'That puck bunny has dated the whole team. <br><br> (Note: Just because a girl likes to play or watch hockey doesn\'t make her a bunny.)'
         },
         {
         question: "Biscuit",
-        choices: ["The Puck"],
+        choices: ["A bad player", "The puck", "An equipment deodorizer", "Goalie treat"],
         qNum : 15,
-        correct : 0,
-        correctImage : ["assets/images/gameanswers/"],
-        correctDescription:'Dish the biscuit!'
+        correct : 1,
+        correctImage : ["assets/images/gameanswers/puck.gif"],
+        correctDescription:'Sauce the biscuit!'
         },
         {
         question: "Clapper",
-        choices: ["A slapshot/very hard shot"],
+        choices: ["An enthusiastic fan", "A slapshot/very hard shot", "Team captain", "Coach"],
         qNum : 16,
-        correct : 0,
-        correctImage : ["assets/images/gameanswers/"],
+        correct : 1,
+        correctImage : ["assets/images/gameanswers/clapper.gif"],
         correctDescription:'Bet his ears are ringing after taking that clapper to the bucket.'
         },
         {
         question: "Bar Down",
-        choices: ["A shot that deflects into the net off of the crossbar."],
+        choices: ["Drinking after the game", "Hustling during your shift", "Thoroughly beating a team.", "A shot that deflects into the net off of the crossbar."],
         qNum : 17,
-        correct : 0,
-        correctImage : ["assets/images/gameanswers/"],
+        correct : 3,
+        correctImage : ["assets/images/gameanswers/bardown.gif"],
         correctDescription:'She went bar down on that clapper!'
         },
         {
         question: "Barn",
-        choices: ["Ice Rick"],
+        choices: ["Locker room", "Ice rink", "The goal", "A place to drink after the game."],
         qNum : 18,
-        correct : 0,
-        correctImage : ["assets/images/gameanswers/"],
+        correct : 1,
+        correctImage : ["assets/images/gameanswers/barn.jpeg"],
         correctDescription:'Goin\' down to the barn to play some hockey.' 
         },
         {
         question: "Celly",
-        choices: ["A celebration"],
+        choices: ["A celebration after a goal", "A jersey", "The Zamboni", "Stick tape"],
         qNum : 19,
         correct : 0,
         correctImage : ["assets/images/gameanswers/"],
@@ -164,43 +164,43 @@
         },
         {
         question: "The Show",
-        choices: ["The NHL"],
+        choices: ["SportsCenter", "A big game", "The NHL", "A top player"],
         qNum : 20,
-        correct : 0,
+        correct : 2,
         correctImage : ["assets/images/gameanswers/"],
         correctDescription:'Every hockey player dreams of going to the The Show.'
         },
         {
         question: "Sauce",
-        choices: ["A pass that goes through the air and hits the ice right before hitting the teammate\'s stick tape."],
+        choices: ["Beer", "A pass that goes through the air and hits the ice right before hitting the teammate\'s stick tape.", "A great goal", "A fast skater"],
         qNum : 21,
-        correct : 0,
+        correct : 1,
         correctImage : ["assets/images/gameanswers/"],
         correctDescription:'He put it where grandma puts the peanut butter after a greasy sauce from winger.'
         },
         {
         question: "Sieve",
-        choices: ["A bad goalie"],
+        choices: ["A bad goalie", "The netting on the goal", "The Zamboni", "The coach"],
         qNum : 22,
         correct : 0,
         correctImage : ["assets/images/gameanswers/"],
-        correctDescription:'That sieve is so full of holes he lets everything in.'
+        correctDescription:'That sieve is so full of holes; he lets everything in.'
         },
         {
         question: "Snipe",
-        choices: [""],
+        choices: ["A great pass", "A big hit", "A player that hangs out at center ice waiting for a breakaway pass.", "A well placed shot that results in a goal."],
         qNum : 23,
-        correct : 0,
-        correctImage : ["assets/images/gameanswers/"],
-        correctDescription:''
+        correct : 3,
+        correctImage : ["assets/images/gameanswers/snipe.gif"],
+        correctDescription:'He snipped that beauty bar down. Made the goalie into a real sieve.'
         },
         {
         question: "Gordie Howe Hat Trick",
-        choices: [""],
+        choices: ["Three Goals", "A goal, an assist, and fight in one game", "Three assists", "Three beers"],
         qNum : 24,
-        correct : 0,
+        correct : 1,
         correctImage : ["assets/images/gameanswers/"],
-        correctDescription:''
+        correctDescription:'With a Gordie Howe Hat Trick, that beauty had a great game.'
         },
         ]
 
